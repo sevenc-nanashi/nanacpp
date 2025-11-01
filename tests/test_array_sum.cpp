81fe2a3e1634c7578188a3cb83edfb9c107a2b4f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-11-01 22:58:12 +0900

    test: テストを追加

--- tests/test_array_sum.cpp
+++ tests/test_array_sum.cpp
@@ -10,6 +10,9 @@ int main() {
   assert(arr_sum.sum(2, 4) == 7);
   assert(arr_sum.sum(1, 1) == 0);
 
+  assert(arr_sum.sum_closed(0, 4) == 15);
+  assert(arr_sum.sum_closed(1, 3) == 9);
+  assert(arr_sum.sum_closed(2, 2) == 3);
+
   return 0;
 }
-
--- tests/test_bisect.cpp
+++ tests/test_bisect.cpp
@@ -3,18 +3,23 @@
 #include <cassert>
 
 int main() {
-  auto first = bisect_first(0, 100, [](u64 x) { return x * x >= 2500; });
+  auto first = bisect_first(0, 100, [](i64 x) { return x * x >= 2500; });
   assert(first == 50);
 
-  auto last = bisect_last(0, 100, [](u64 x) { return x * x <= 2500; });
+  auto last = bisect_last(0, 100, [](i64 x) { return x * x <= 2500; });
   assert(last == 50);
 
-  auto first_false = bisect_first(0, 10, [](u64 x) { return x >= 10; });
-  assert(first_false == 10);
+  auto first_negative = bisect_first(-5, 5, [](i64 x) { return x >= -2; });
+  assert(first_negative == -2);
 
-  auto last_false = bisect_last(0, 10, [](u64 x) { return x <= 0; });
-  assert(last_false == 0);
+  auto last_negative = bisect_last(-5, 5, [](i64 x) { return x <= -2; });
+  assert(last_negative == -2);
+
+  auto first_not_found = bisect_first(-5, 5, [](i64 x) { return x > 5; });
+  assert(first_not_found == 6);
+
+  auto last_not_found = bisect_last(-5, 5, [](i64 x) { return x < -10; });
+  assert(last_not_found == -6);
 
   return 0;
 }
-
